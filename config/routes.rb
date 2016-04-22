4fed8b7523f24c9b279b6be8eba54930aacb7baa
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-04-22 17:20:32 -0500

    pre-migration card, board, memberships

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root to: "users#home"
+  root to: "users#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ db/migrate/20160422221306_create_join_table_membership.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableMembership < ActiveRecord::Migration
+  def change
+    create_join_table :users, :boards, table_name: :categorization do |t|
+      t.index :user_id
+      t.index :board_id
+    end
+  end
+end
