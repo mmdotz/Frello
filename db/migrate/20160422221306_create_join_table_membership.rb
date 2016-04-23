5009ebcf120cf1ef3af4335bba1494fb918c0872
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-04-23 12:29:29 -0500

    add column and remigrate join table

--- db/migrate/20160422221306_create_join_table_membership.rb
+++ db/migrate/20160422221306_create_join_table_membership.rb
@@ -1,8 +1,9 @@
 class CreateJoinTableMembership < ActiveRecord::Migration
   def change
-    create_join_table :users, :boards, table_name: :categorization do |t|
+    create_join_table :users, :boards, table_name: :membership do |t|
       t.index :user_id
       t.index :board_id
+      t.boolean :owner, default: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405150059) do
+ActiveRecord::Schema.define(version: 20160422221306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20160405150059) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "membership", id: false, force: :cascade do |t|
+    t.integer "user_id",                  null: false
+    t.integer "board_id",                 null: false
+    t.boolean "owner",    default: false
+  end
+
+  add_index "membership", ["board_id"], name: "index_membership_on_board_id", using: :btree
+  add_index "membership", ["user_id"], name: "index_membership_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
