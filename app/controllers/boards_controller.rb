b6e16b84f33859df5ff3ab91e3943d3864d96a66
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-04-23 12:01:00 -0500

    build out board controller...again

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,16 +1,41 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: [:show, :create, :destroy]
+
   def index
+    @boards = current_user.boards
   end
 
   def new
+  end
+
+  def show
+    @board = Board.find(params[:id])
   end
 
   def edit
   end
 
   def create
+    @board = Board.new(board_params)
+    respond_to do |format|
+      if @board.save
+        format.html { redirect_to item_path(@board), notice: 'You created a new Frello Board!' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def destroy
+  end
+
+  protected
+
+  def set_board
+    @board = Board.find(params[:id])
+  end
+
+  def board_params
+    params.require(:board).permit(:name, :setting, :category)
   end
 end
