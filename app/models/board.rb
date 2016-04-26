f611a29e64cf20f3136259d0924df49354b7f5c7
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-04-26 09:59:50 -0500

    card associations, join relation on board?

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,5 @@
 class Board < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user #scope owner is true?
+  has_many   :members, class_name: "User", foreign_key: "member_id"
   has_many   :cards
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,7 @@
 class Card < ActiveRecord::Base
+  belongs_to :board
+  has_many :comments
+  has_many :checklists
+  has_many :members 
 
 end
