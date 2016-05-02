11109d6f502e5289b4870822c282641e5fbb57a8
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-05-01 22:37:01 -0500

    fix partial name, update new board controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,6 +6,8 @@ class BoardsController < ApplicationController
   end
 
   def new
+    @board = Board.new
+    @user = current_user
   end
 
   def show
@@ -19,7 +21,7 @@ class BoardsController < ApplicationController
     @board = Board.new(board_params)
     respond_to do |format|
       if @board.save
-        format.html { redirect_to board_path(@board), notice: 'You created a new Frello Board!' }
+        format.html { redirect_to @board, notice: 'You created a new Frello Board!' }
       else
         format.html { render :new }
       end
